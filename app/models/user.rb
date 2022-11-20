f4067c724b7d584eefbb45c6cb1c63a049280390
sea-yassan33 <a.yasuda_tr51@me.com>
2022-11-20 18:20:07 +0900

    修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,14 @@ class User < ApplicationRecord
     validates :password,
               format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
                         message: 'is invalid. Include both letters and numbers' }
+    validates :nickname
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
     validates :last_name_kana,
               format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
     validates :first_name_kana,
               format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
+    validates :birth_date
   end
-
-  has_many :items
-  has_many :orders
-
-  validates :password, confirmation: true
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+  
 end
