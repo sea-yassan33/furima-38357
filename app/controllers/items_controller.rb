409a109798db5974223b7739eae3a19f13494857
sea-yassan33 <a.yasuda_tr51@me.com>
2022-11-23 22:17:29 +0900

    保存、バリエーション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,23 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render 'new'
+    end
+  end
+
   private
   def move_to_index
     unless user_signed_in?
       redirect_to(user_session_path)
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,20 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+    validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+    validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+    validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :price, numericality: {only_integer: true ,in: 300..9999999}
+    validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :item_category
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :scheduled_delivery
   belongs_to :prefecture
 
   belongs_to :user
