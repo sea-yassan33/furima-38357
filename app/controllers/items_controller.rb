1be0ab41826949f12903560785acd1c262c6c8b9
sea-yassan33 <a.yasuda_tr51@me.com>
2022-11-27 00:48:29 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
   before_action :corrent_user, only: [:edit]
-  before_action :find_item, only: [:show, :edit, :update, :destroy]
-  
+  before_action :find_item, only: %i[show edit update destroy]
+
   def index
     @item = Item.all.order('created_at DESC')
   end
@@ -20,11 +20,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @item.update(item_params)
@@ -43,8 +41,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id,
                                  :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
@@ -54,10 +52,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @user = @item.user
     return if @user == current_user
+
     redirect_to(items_path)
   end
 
   def find_item
     @item = Item.find(params[:id])
-  end 
+  end
 end
