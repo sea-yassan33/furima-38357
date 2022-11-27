23960652437241ececadf3c9aa51c28e83b05bd9
sea-yassan33 <a.yasuda_tr51@me.com>
2022-11-28 00:04:13 +0900

    購入画面へ遷移_修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,23 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+   @item = Item.find(params[:item_id])
   end
 
   def create
-
+    @order = Order.new(order_params)
+    Address.create(address_params)
+    redirect_to root_path
   end
-  
+
+  private
+  def order_params
+    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def address_params
+    params.require(:address).permit(:postal_code, :prefecture_id, :city, :address,
+       :building, :phone_number).merge(order_id: @order.id)
+  end
+
 end
