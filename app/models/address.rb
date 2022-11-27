ec544b2f1ca3c88225288d95339bcca95e1eb9c5
sea-yassan33 <a.yasuda_tr51@me.com>
2022-11-27 18:41:46 +0900

    バリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,10 @@
 class Address < ApplicationRecord
   with_options presence: true do
+    validates :postal_code, numericality: {with: /\A\d{3}-\d{4}\z/, message: "code is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number ,length:{minimum: 10, maximum: 11 },numericality:{with:/\A\d\z/, message: "number is invalid. Input only number"}
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
