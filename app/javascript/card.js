f6330e924993713e757fb17874e17ebc44beabc4
sea-yassan33 <a.yasuda_tr51@me.com>
2022-12-02 05:09:08 +0900

    カード情報のトークン化

---
+++ app/javascript/card.js
@@ -0,0 +1,25 @@
+const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_form[number]"),
+      cvc: formData.get("order_form[cvc]"),
+      exp_month: formData.get("order_form[exp_month]"),
+      exp_year: `20${formData.get("order_form[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
